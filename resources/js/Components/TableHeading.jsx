66935ed32f293990c135c858411c83a36b75ac96
Thilina Madhusanka <120771707+nkthilina@users.noreply.github.com>
2024-04-27 17:12:34 +0530

    8th commit

---
+++ resources/js/Components/TableHeading.jsx
@@ -0,0 +1,49 @@
+import React, { children } from "react";
+import { ChevronUpIcon, ChevronDownIcon } from "@heroicons/react/24/solid";
+
+function TableHeading({
+  name,
+  sortable = true,
+  sort_field = null,
+  sort_direction = null,
+  sortChanged = () => {},
+  children,
+}) {
+  return (
+      <th
+        className="py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
+      >
+        <div className="flex items-center justify-between gap-1">
+        <div className="text-nowrap">
+            {children}
+            {/* {sort_field} */}
+            {/* {sort_direction} */}
+        </div>
+        <div>
+            {sortable && (
+              <div  onClick={(e) => sortChanged(name)}>
+                <ChevronUpIcon
+                  className={
+                    "w-4 h-4 mx-auto" +
+                    (sort_field === name && sort_direction === "asc"
+                      ? " text-white w-5 h-5"
+                      : "")
+                  }
+                />
+                <ChevronDownIcon
+                  className={
+                    "w-4 h-4 -mt-2 mx-auto" +
+                    (sort_field === name && sort_direction === "desc"
+                      ? " text-white w-5 h-5"
+                      : "")
+                  }
+                />
+              </div>
+            )}
+            </div>
+        </div>
+      </th>
+  );
+}
+
+export default TableHeading;
--- resources/js/Pages/Project/Index.jsx
+++ resources/js/Pages/Project/Index.jsx
@@ -10,6 +10,7 @@ import {
 import TextInput from "@/Components/TextInput";
 import SelectInput from "@/Components/SelectInput";
 import { ChevronUpIcon, ChevronDownIcon } from "@heroicons/react/24/solid";
+import TableHeading from "@/Components/TableHeading";
 //import { PROJECT_STATUS_CLASS_MAP, PROJECT_STATUS_TEXT_MAP } from "@/constants.jsx";
 
 function Index({ auth, projects, queryParams = null }) {
@@ -66,128 +67,6 @@ function Index({ auth, projects, queryParams = null }) {
         </div>
       </div> */}
 
-      {/* <div className="mt-12">
-        <div className="mb-12 grid gap-y-10 gap-x-6 md:grid-cols-2 xl:grid-cols-4">
-          <div className="relative flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 shadow-md">
-                <div className="bg-clip-border mx-4 rounded-xl overflow-hidden bg-gradient-to-tr from-blue-600 to-blue-400 text-white shadow-blue-500/40 shadow-lg absolute -mt-4 grid h-16 w-16 place-items-center">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                    aria-hidden="true"
-                    className="w-6 h-6 text-white"
-                  >
-                    <path d="M12 7.5a2.25 2.25 0 100 4.5 2.25 2.25 0 000-4.5z" />
-                    <path
-                      fillRule="evenodd"
-                      d="M1.5 4.875C1.5 3.839 2.34 3 3.375 3h17.25c1.035 0 1.875.84 1.875 1.875v9.75c0 1.036-.84 1.875-1.875 1.875H3.375A1.875 1.875 0 011.5 14.625v-9.75zM8.25 9.75a3.75 3.75 0 117.5 0 3.75 3.75 0 01-7.5 0zM18.75 9a.75.75 0 00-.75.75v.008c0 .414.336.75.75.75h.008a.75.75 0 00.75-.75V9.75a.75.75 0 00-.75-.75h-.008zM4.5 9.75A.75.75 0 015.25 9h.008a.75.75 0 01.75.75v.008a.75.75 0 01-.75.75H5.25a.75.75 0 01-.75-.75V9.75z"
-                      clipRule="evenodd"
-                    />
-                    <path d="M2.25 18a.75.75 0 000 1.5c5.4 0 10.63.722 15.6 2.075 1.19.324 2.4-.558 2.4-1.82V18.75a.75.75 0 00-.75-.75H2.25z" />
-                  </svg>
-                </div>
-                <div className="p-4 text-right">
-                  <p className="block antialiased font-sans text-sm leading-normal font-normal text-blue-gray-600">
-                    Today's Money
-                  </p>
-                  <h4 className="block antialiased tracking-normal font-sans text-2xl font-semibold leading-snug text-blue-gray-900">
-                    $53k
-                  </h4>
-                </div>
-                <div className="border-t border-blue-gray-50 p-4">
-                  <p className="block antialiased font-sans text-base leading-relaxed font-normal text-blue-gray-600">
-                    <strong className="text-green-500">+55%</strong>&nbsp;than
-                    last week
-                  </p>
-                </div>
-              </div>
-          <div className="relative flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 shadow-md">
-                <div className="bg-clip-border mx-4 rounded-xl overflow-hidden bg-gradient-to-tr from-pink-600 to-pink-400 text-white shadow-pink-500/40 shadow-lg absolute -mt-4 grid h-16 w-16 place-items-center">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                    aria-hidden="true"
-                    className="w-6 h-6 text-white"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                </div>
-                <div className="p-4 text-right">
-                  <p className="block antialiased font-sans text-sm leading-normal font-normal text-blue-gray-600">
-                    Today's Users
-                  </p>
-                  <h4 className="block antialiased tracking-normal font-sans text-2xl font-semibold leading-snug text-blue-gray-900">
-                    2,300
-                  </h4>
-                </div>
-                <div className="border-t border-blue-gray-50 p-4">
-                  <p className="block antialiased font-sans text-base leading-relaxed font-normal text-blue-gray-600">
-                    <strong className="text-green-500">+3%</strong>&nbsp;than
-                    last month
-                  </p>
-                </div>
-              </div>
-          <div className="relative flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 shadow-md">
-                <div className="bg-clip-border mx-4 rounded-xl overflow-hidden bg-gradient-to-tr from-green-600 to-green-400 text-white shadow-green-500/40 shadow-lg absolute -mt-4 grid h-16 w-16 place-items-center">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                    aria-hidden="true"
-                    className="w-6 h-6 text-white"
-                  >
-                    <path d="M6.25 6.375a4.125 4.125 0 118.25 0 4.125 4.125 0 01-8.25 0zM3.25 19.125a7.125 7.125 0 0114.25 0v.003l-.001.119a.75.75 0 01-.363.63 13.067 13.067 0 01-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 01-.364-.63l-.001-.122zM19.75 7.5a.75.75 0 00-1.5 0v2.25H16a.75.75 0 000 1.5h2.25v2.25a.75.75 0 001.5 0v-2.25H22a.75.75 0 000-1.5h-2.25V7.5z" />
-                  </svg>
-                </div>
-                <div className="p-4 text-right">
-                  <p className="block antialiased font-sans text-sm leading-normal font-normal text-blue-gray-600">
-                    New Clients
-                  </p>
-                  <h4 className="block antialiased tracking-normal font-sans text-2xl font-semibold leading-snug text-blue-gray-900">
-                    3,462
-                  </h4>
-                </div>
-                <div className="border-t border-blue-gray-50 p-4">
-                  <p className="block antialiased font-sans text-base leading-relaxed font-normal text-blue-gray-600">
-                    <strong className="text-red-500">-2%</strong>&nbsp;than
-                    yesterday
-                  </p>
-                </div>
-              </div>
-          <div className="relative flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 shadow-md">
-                <div className="bg-clip-border mx-4 rounded-xl overflow-hidden bg-gradient-to-tr from-orange-600 to-orange-400 text-white shadow-orange-500/40 shadow-lg absolute -mt-4 grid h-16 w-16 place-items-center">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                    aria-hidden="true"
-                    className="w-6 h-6 text-white"
-                  >
-                    <path d="M18.375 2.25c-1.035 0-1.875.84-1.875 1.875v15.75c0 1.035.84 1.875 1.875 1.875h.75c1.035 0 1.875-.84 1.875-1.875V4.125c0-1.036-.84-1.875-1.875-1.875h-.75zM9.75 8.625c0-1.036.84-1.875 1.875-1.875h.75c1.036 0 1.875.84 1.875 1.875v11.25c0 1.035-.84 1.875-1.875 1.875h-.75a1.875 1.875 0 01-1.875-1.875V8.625zM3 13.125c0-1.036.84-1.875 1.875-1.875h.75c1.036 0 1.875.84 1.875 1.875v6.75c0 1.035-.84 1.875-1.875 1.875h-.75A1.875 1.875 0 013 19.875v-6.75z" />
-                  </svg>
-                </div>
-                <div className="p-4 text-right">
-                  <p className="block antialiased font-sans text-sm leading-normal font-normal text-blue-gray-600">
-                    Sales
-                  </p>
-                  <h4 className="block antialiased tracking-normal font-sans text-2xl font-semibold leading-snug text-blue-gray-900">
-                    $103,430
-                  </h4>
-                </div>
-                <div className="border-t border-blue-gray-50 p-4">
-                  <p className="block antialiased font-sans text-base leading-relaxed font-normal text-blue-gray-600">
-                    <strong className="text-green-500">+5%</strong>&nbsp;than
-                    yesterday
-                  </p>
-                </div>
-              </div>
-        </div>
-      </div> */}
 
       {/* new table */}
       <section className="container px-4 mx-auto mt-5">
@@ -267,97 +146,45 @@ function Index({ auth, projects, queryParams = null }) {
                   </thead>
                   <thead className="bg-gray-50 dark:bg-gray-800">
                     <tr>
-                      <th
-                        scope="col"
-                        className="py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
-                      >
-                        <div className="flex items-center gap-x-3">
-                          <button className="flex items-center gap-x-2">
-                            <span>ID</span>
-                            <svg
-                              onClick={(e) => sortChanged("id")}
-                              className="h-3 cursor-pointer"
-                              viewBox="0 0 10 11"
-                              fill="none"
-                              xmlns="http://www.w3.org/2000/svg"
-                            >
-                              <path
-                                d="M2.13347 0.0999756H2.98516L5.01902 4.79058H3.86226L3.45549 3.79907H1.63772L1.24366 4.79058H0.0996094L2.13347 0.0999756ZM2.54025 1.46012L1.96822 2.92196H3.11227L2.54025 1.46012Z"
-                                fill="currentColor"
-                                stroke="currentColor"
-                                strokeWidth="0.1"
-                              />
-                              <path
-                                d="M0.722656 9.60832L3.09974 6.78633H0.811638V5.87109H4.35819V6.78633L2.01925 9.60832H4.43446V10.5617H0.722656V9.60832Z"
-                                fill="currentColor"
-                                stroke="currentColor"
-                                strokeWidth="0.1"
-                              />
-                              <path
-                                d="M8.45558 7.25664V7.40664H8.60558H9.66065C9.72481 7.40664 9.74667 7.42274 9.75141 7.42691C9.75148 7.42808 9.75146 7.42993 9.75116 7.43262C9.75001 7.44265 9.74458 7.46304 9.72525 7.49314C9.72522 7.4932 9.72518 7.49326 9.72514 7.49332L7.86959 10.3529L7.86924 10.3534C7.83227 10.4109 7.79863 10.418 7.78568 10.418C7.77272 10.418 7.73908 10.4109 7.70211 10.3534L7.70177 10.3529L5.84621 7.49332C5.84617 7.49325 5.84612 7.49318 5.84608 7.49311C5.82677 7.46302 5.82135 7.44264 5.8202 7.43262C5.81989 7.42993 5.81987 7.42808 5.81994 7.42691C5.82469 7.42274 5.84655 7.40664 5.91071 7.40664H6.96578H7.11578V7.25664V0.633865C7.11578 0.42434 7.29014 0.249976 7.49967 0.249976H8.07169C8.28121 0.249976 8.45558 0.42434 8.45558 0.633865V7.25664Z"
-                                fill="currentColor"
-                                stroke="currentColor"
-                                strokeWidth="0.3"
-                              />
-                            </svg>
-                          </button>
-                        </div>
-                      </th>
+                      <TableHeading
+                      name="id"
+                        sort_field={queryParams.sort_field}
+                        sort_direction={queryParams.sort_direction}
+                        sortChanged={sortChanged}
+                      > ID </TableHeading>
                       <th
                         scope="col"
                         className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
                       >
                         Image
                       </th>
-                      <th
-                        scope="col"
-                        className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400 "
-                      >
-                        <div className="flex items-center justify-between">
-                          Name
-                          <div onClick={(e) => sortChanged("name")}>
-                            <ChevronUpIcon className="w-4 h-4" />
-                            <ChevronDownIcon className="w-4 h-4 -mt-1" />
-                          </div>
-                        </div>
-                      </th>
-                      <th
-                        scope="col"
-                        className="px-5 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
-                      >
-                        <div className="flex items-center justify-between">
-                          Status
-                          <div onClick={(e) => sortChanged("status")}>
-                            <ChevronUpIcon className="w-4 h-4" />
-                            <ChevronDownIcon className="w-4 h-4 -mt-1" />
-                          </div>
-                        </div>
-                      </th>
-                      <th
-                        scope="col"
-                        className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
-                      >
-                        <div className="flex items-center justify-between text-nowrap gap-1">
-                          Created date
-                          <div onClick={(e) => sortChanged("created_at")}>
-                            <ChevronUpIcon className="w-4 h-4" />
-                            <ChevronDownIcon className="w-4 h-4 -mt-1" />
-                          </div>
-                        </div>
-                      </th>
-                      <th
+                      <TableHeading
+                      name="name"
+                        sort_field={queryParams.sort_field}
+                        sort_direction={queryParams.sort_direction}
+                        sortChanged={sortChanged}
+                      > Name </TableHeading>
+                      <TableHeading
+                      name="status"
+                        sort_field={queryParams.sort_field}
+                        sort_direction={queryParams.sort_direction}
+                        sortChanged={sortChanged}
+                      > Status </TableHeading>
+                      <TableHeading
+                      name="created_date"
+                        sort_field={queryParams.sort_field}
+                        sort_direction={queryParams.sort_direction}
+                        sortChanged={sortChanged}
+                      > Created date </TableHeading>
+                      <TableHeading
+                      name="due_date"
+                        sort_field={queryParams.sort_field}
+                        sort_direction={queryParams.sort_direction}
+                        sortChanged={sortChanged}
+                      > Due date </TableHeading>
 
-                        scope="col"
-                        className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
-                      >
-                      <div className="flex items-center justify-between" >
-                        Due date
-                        <div onClick={(e) => sortChanged("due_date")}>
-                            <ChevronUpIcon className="w-4 h-4" />
-                            <ChevronDownIcon className="w-4 h-4 -mt-1" />
-                          </div>
-                      </div>
-                      </th>
+
+                      
                       <th
                         scope="col"
                         className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400 text-nowrap"
