efe2381e1621fa8996cd21858bfbed62c90f676d
Thilina Madhusanka <120771707+nkthilina@users.noreply.github.com>
2024-04-30 19:42:15 +0530

    10th commit

--- resources/js/Pages/Project/Show.jsx
+++ resources/js/Pages/Project/Show.jsx
@@ -15,7 +15,7 @@ function Show({ auth, project }) {
       {/* <pre>{JSON.stringify(project, null, 2)}</pre> */}
       <section className="container px-4 mx-auto mt-5">
         <div className="flex flex-col">
-          <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="-mx-4 -my-2 overflow-x-auto overflow-y-auto sm:-mx-6 lg:-mx-8">
             <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
               <div className="overflow-hidden border border-gray-200 dark:border-gray-700 md:rounded-lg ">
                 <div className="p-6">
