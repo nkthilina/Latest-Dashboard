de18db22c1c4193543fa13f643fd0476c9fe6a96
Thilina Madhusanka <120771707+nkthilina@users.noreply.github.com>
2024-05-01 23:43:59 +0530

    11th commit

--- resources/js/Pages/Project/Show.jsx
+++ resources/js/Pages/Project/Show.jsx
@@ -13,9 +13,9 @@ function Show({ auth, project }) {
     >
       {/* <Head title={`Project "${project.name}"`} /> */}
       {/* <pre>{JSON.stringify(project, null, 2)}</pre> */}
-      <section className="container px-4 mx-auto mt-5">
-        <div className="flex flex-col">
-          <div className="-mx-4 -my-2 overflow-x-auto overflow-y-auto sm:-mx-6 lg:-mx-8">
+      <section className="container px-4 mx-auto mt-5 ">
+        <div className="flex flex-col bg-black">
+          <div className="-mx-4 -my-2 overflow-x-auto overflow-y-auto sm:-mx-6 lg:-mx-8 ">
             <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
               <div className="overflow-hidden border border-gray-200 dark:border-gray-700 md:rounded-lg ">
                 <div className="p-6">
@@ -25,6 +25,29 @@ function Show({ auth, project }) {
                     className="w-full h-64 object-cover"
                   />
                 </div>
+                <div className="p-6 grid gap-1 grid-cols-2 mt-2">
+                  <div>
+                    <div className="">
+                      <label
+                        className="block text-sm font-medium text-gray-700 dark:text-gray-300"
+                        htmlFor="name"
+                      >
+                        Project ID
+                      </label>
+                      <p className="mt-1">{project.id}</p>
+                      </div>
+                  </div>
+                    <div className="">
+                      <label
+                        className="block text-sm font-medium text-gray-700 dark:text-gray-300"
+                        htmlFor="name"
+                      >
+                        Name
+                      </label>
+                      <p className="mt-1">{project.name}</p>
+                      </div>
+                  </div>
+                  <div></div>
               </div>
             </div>
           </div>
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\UserController;
-use Illuminate\Foundation\Application;
+// use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
