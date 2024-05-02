cbda8467f8544695efe6b36ccf6088d086ee4b75
Thilina Madhusanka <120771707+nkthilina@users.noreply.github.com>
2024-05-02 09:13:20 +0530

    12th commit

--- resources/js/Pages/Project/Show.jsx
+++ resources/js/Pages/Project/Show.jsx
@@ -1,5 +1,10 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import React from "react";
+import {
+  PROJECT_STATUS_CLASS_MAP,
+  PROJECT_STATUS_TEXT_MAP,
+} from "@/constants.jsx";
+import { Head } from "@inertiajs/react";
 
 function Show({ auth, project }) {
   return (
@@ -34,20 +39,99 @@ function Show({ auth, project }) {
                       >
                         Project ID
                       </label>
-                      <p className="mt-1">{project.id}</p>
-                      </div>
-                  </div>
+                      <p className="mt-1 block text-sm font-medium text-gray-700 dark:text-gray-300">
+                        {project.id}
+                      </p>
+                    </div>
                     <div className="">
                       <label
                         className="block text-sm font-medium text-gray-700 dark:text-gray-300"
                         htmlFor="name"
                       >
-                        Name
+                        Project Name
                       </label>
-                      <p className="mt-1">{project.name}</p>
-                      </div>
+                      <p className="mt-1 block text-sm font-medium text-gray-700 dark:text-gray-300">
+                        {project.name}
+                      </p>
+                    </div>
+                    <div className="">
+                      <label
+                        className="block text-sm font-medium text-gray-700 dark:text-gray-300"
+                        htmlFor="name"
+                      >
+                        Project Status
+                      </label>
+                      <p className="mt-1 block text-sm font-medium text-gray-700 dark:text-gray-300">
+                        <span
+                          className={
+                            "text-sm font-medium  whitespace-nowrap inline-flex items-center px-3 py-1 rounded-full gap-x-2  bg-gray-800 " +
+                            PROJECT_STATUS_CLASS_MAP[project.status]
+                          }
+                        >
+                          {PROJECT_STATUS_TEXT_MAP[project.status]}
+                        </span>
+                      </p>
+                    </div>
+                    <div className="">
+                      <label
+                        className="block text-sm font-medium text-gray-700 dark:text-gray-300"
+                        htmlFor="name"
+                      >
+                        Created By
+                      </label>
+                      <p className="mt-1 block text-sm font-medium text-gray-700 dark:text-gray-300">
+                        {project.createdBy.name}
+                      </p>
+                    </div>
                   </div>
-                  <div></div>
+                  <div>
+                    <div className="">
+                      <label
+                        className="block text-sm font-medium text-gray-700 dark:text-gray-300"
+                        htmlFor="name"
+                      >
+                        Due Date
+                      </label>
+                      <p className="mt-1 block text-sm font-medium text-gray-700 dark:text-gray-300">
+                        {project.due_date}
+                      </p>
+                    </div>
+                    <div className="">
+                      <label
+                        className="block text-sm font-medium text-gray-700 dark:text-gray-300"
+                        htmlFor="name"
+                      >
+                        Create Date
+                      </label>
+                      <p className="mt-1 block text-sm font-medium text-gray-700 dark:text-gray-300">
+                        {project.created_at}
+                      </p>
+                    </div>
+                    <div className="">
+                      <label
+                        className="block text-sm font-medium text-gray-700 dark:text-gray-300"
+                        htmlFor="name"
+                      >
+                        Updated By
+                      </label>
+                      <p className="mt-1 block text-sm font-medium text-gray-700 dark:text-gray-300">
+                        {project.updatedBy.name}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="space-y-1">
+                      <label
+                        className="block text-sm font-medium text-gray-700 dark:text-gray-300"
+                        htmlFor="name"
+                      >
+                        Description
+                      </label>
+                      <p className="mt-1 block text-sm font-medium text-gray-700 dark:text-gray-300">
+                        {project.description}
+                      </p>
+                  </div>
+
               </div>
             </div>
           </div>
