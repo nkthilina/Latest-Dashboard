e370fc7c1da9b9b2afec5ae868c6cc402a80c342
Thilina Madhusanka <120771707+nkthilina@users.noreply.github.com>
2024-06-16 18:54:24 +0530

    63rd commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -50,8 +52,16 @@ class User extends Authenticatable
         ];
     }
 
-    public function isAdmin()
-    {
-        return $this->role === 'admin';
-    }
+    /**
+    * Interact with the user's first name.
+    *
+    * @param  string  $value
+    * @return \Illuminate\Database\Eloquent\Casts\Attribute
+    */
+   protected function type(): Attribute
+   {
+       return new Attribute(
+           get: fn ($value) => ["owner", "manager", "cashier"][$value],
+       );
+   }
 }
