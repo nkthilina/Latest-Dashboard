61e0c62850516bb16da9bacdde7a39e920e95594
Thilina Madhusanka <120771707+nkthilina@users.noreply.github.com>
2024-06-18 11:00:07 +0530

    67th commit

--- resources/js/Components/Footer.jsx
+++ resources/js/Components/Footer.jsx
@@ -46,7 +46,7 @@ function Footer() {
               GitHub
             </a>
           </li>
-          <li>
+          {/* <li>
             <a
               href="https://www.creative-tim.com/blog"
               target="_blank"
@@ -54,89 +54,87 @@ function Footer() {
             >
               About us
             </a>
+          </li> */}
+
+          {/* testing */}
+          <li>
+            {/* Button to open the modal */}
+            <button
+              onClick={() => setShowModal(true)}
+              type="button"
+              className="block antialiased font-sans text-sm leading-normal py-0.5 px-1 font-normal text-inherit transition-colors hover:text-blue-500"
+            >
+              About us
+            </button>
           </li>
-          {/* <li>
-              <a
-                href="https://www.creative-tim.com/license"
-                target="_blank"
-                className="block antialiased font-sans text-sm leading-normal py-0.5 px-1 font-normal text-inherit transition-colors hover:text-blue-500"
-              >
-                LinkedIn
-              </a>
-            </li> */}
+          {/* end testing */}
         </ul>
       </div>
 
-      {/* testing */}
-      <div className="flex items-center justify-center h-screen">
-        {/* Button to open the modal */}
-        <button
-          onClick={() => setShowModal(true)}
-          className="w-full px-4 py-2 text-sm text-white font-medium  bg-red-500 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-red-500"
-        >
-          {" "}
-          Delete Sample Item{" "}
-        </button>
+      {/* Modal component */}
+      <div className="flex items-center justify-center ">
         {showModal && (
-        {/* Background overlay */}
-        <div
-          className="fixed inset-0 transition-opacity"
-          aria-hidden="true"
-        >
-          <div className="absolute inset-0 bg-gray-500 opacity-75" />
-        </div>
-        {/* Modal */}
-        <div
-          className="fixed z-10 inset-0 overflow-y-auto"
-        >
-          <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-            {/* Modal panel */}
-            <div
-              className="w-full inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
-              role="dialog"
-              aria-modal="true"
-              aria-labelledby="modal-headline"
-            >
-              <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                {/* Modal content */}
-                <div className="sm:flex sm:items-start">
-
-                  <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                    <h3
-                      className="text-lg leading-6 font-medium text-gray-900"
-                      id="modal-headline"
-                    >
-                      {" "}
-                      Delete Item{" "}
-                    </h3>
-                    <div className="mt-2">
-                      <p className="text-sm text-gray-500">
-                        {" "}
-                        Are you sure you want to delete{" "}
-                        <span className="font-bold">Sample Item</span>? This
-                        action cannot be undone.{" "}
-                      </p>
+          <div>
+            {/* Background overlay */}
+            <div className="fixed z-10 inset-0 overflow-y-auto">
+              <div className="absolute inset-0 bg-gray-500 opacity-75">fu</div>
+            </div>
+            {/* Modal */}
+            <div className="fixed z-10 inset-0 overflow-y-auto ">
+              <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0 ">
+                {/* Modal panel */}
+                <div
+                  className="w-full inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
+                  role="dialog"
+                  aria-modal="true"
+                  aria-labelledby="modal-headline"
+                >
+                  <div className="bg-gray-50 px-4 pt-5 pb-4 sm:p-6 sm:pb-4 ">
+                    {/* Modal content */}
+                    <div className="sm:flex sm:items-start">
+                      <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left sm:flex-row-reverse">
+                        <button
+                          onClick={() => setShowModal(false)}
+                          className="absolute top-1 right-0 pt-4 pr-4"
+                        >
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            class="size-6"
+                          >
+                            <path
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              d="M6 18 18 6M6 6l12 12"
+                            />
+                          </svg>
+                        </button>
+                        <h3
+                          className="text-lg leading-6 font-medium text-gray-900"
+                          id="modal-headline"
+                        >
+                          Delete Item{" "}
+                        </h3>
+                        <div className="mt-2">
+                          <p className="text-sm text-gray-500">
+                            Are you sure you want to delete{" "}
+                            <span className="font-bold">Sample Item</span>? This
+                            action cannot be undone.{" "}
+                          </p>
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-              <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-              <button
-                  onClick={() => setShowModal(false)}
-                  type="button"
-                  className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-200 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-                >
-                  {" "}
-                  Cancel{" "}
-                </button>
-              </div>
             </div>
           </div>
-        </div>
-
         )}
       </div>
-      {/* end testing */}
+      {/* Modal component end */}
     </footer>
   );
 }
