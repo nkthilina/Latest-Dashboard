afbb231d44844e5cbc5b5774e8cc05f39ca437fc
Thilina Madhusanka <120771707+nkthilina@users.noreply.github.com>
2024-06-23 00:59:18 +0530

    79th commit

--- resources/js/Components/Footer.jsx
+++ resources/js/Components/Footer.jsx
@@ -114,7 +114,6 @@ function Footer() {
                               <img
                                 alt=""
                                 src={myImage}
-                                // src="https://images.unsplash.com/photo-1587614382346-4ec70e388b28?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
                                 className="h-80 w-60 rounded-3xl object-cover"
                               />
                             </div>
