a42f88e53c5be17dc628fa4a928f67ae58c6fb2e
Thilina Madhusanka <120771707+nkthilina@users.noreply.github.com>
2024-06-28 23:33:28 +0530

    89th commit

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -43,33 +43,11 @@ export default function AuthenticatedLayout({ user, header, children }) {
       <div className="min-h-screen bg-gray-50/50 ">
         <aside className="bg-gradient-to-br from-gray-800 to-gray-900 -translate-x-80 fixed inset-0 z-50 my-4 ml-4 h-[calc(100vh-32px)] w-72 rounded-xl transition-transform duration-300 xl:translate-x-0">
           <div className="relative border-b border-white/20">
-            <a className="flex items-center gap-4 py-6 px-8" href="#/">
+            <div className="flex items-center gap-4 py-6 px-8" href="#/">
               <h6 className="block antialiased tracking-normal font-sans text-base font-semibold leading-relaxed text-white">
                 Admin Nexus
               </h6>
-            </a>
-            <button
-              className="middle none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-8 max-w-[32px] h-8 max-h-[32px] rounded-lg text-xs text-white hover:bg-white/10 active:bg-white/30 absolute right-0 top-0 grid rounded-br-none rounded-tl-none xl:hidden"
-              type="button"
-            >
-              <span className="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="2.5"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                  className="h-5 w-5 text-white"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              </span>
-            </button>
+            </div>
           </div>
           <div className="m-4">
             <ul className="mb-4 flex flex-col gap-1">
@@ -104,7 +82,7 @@ export default function AuthenticatedLayout({ user, header, children }) {
                   className="block antialiased font-sans  leading-relaxed text-inherit capitalize group"
                 >
                   <button
-                    className="middle none font-sans  center transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-base py-3  rounded-lg text-white group-hover:bg-white/10 active:bg-white/30 w-full flex items-center gap-4 px-4 capitalize"
+                    className="middle none font-sans  center transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-base py-3  rounded-lg text-white group-hover:bg-white/10 group-hover:duration-600 active:bg-white/30 w-full flex items-center gap-4 px-4 capitalize"
                     type="button"
                   >
                     <svg
@@ -113,7 +91,7 @@ export default function AuthenticatedLayout({ user, header, children }) {
                       viewBox="0 0 24 24"
                       strokeWidth="1.5"
                       stroke="currentColor"
-                      className="size-5 group-hover:size-6 text-inherit hover:mr-0"
+                      className="size-5 group-hover:size-6 text-inherit "
                     >
                       <path
                         strokeLinecap="round"
@@ -132,7 +110,7 @@ export default function AuthenticatedLayout({ user, header, children }) {
                   className="block antialiased font-sans  leading-relaxed text-inherit capitalize group"
                 >
                   <button
-                    className="middle none font-sans  center transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-base py-3 rounded-lg text-white group-hover:bg-white/10 active:bg-white/30 w-full flex items-center gap-4 px-4 capitalize"
+                    className="middle none font-sans  center transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-base py-3 rounded-lg text-white group-hover:bg-white/10 group-hover:duration-600 active:bg-white/30 w-full flex items-center gap-4 px-4 capitalize"
                     type="button"
                   >
                     <svg
@@ -140,7 +118,7 @@ export default function AuthenticatedLayout({ user, header, children }) {
                       viewBox="0 0 24 24"
                       fill="currentColor"
                       aria-hidden="true"
-                      className="size-5 group-hover:size-6 text-inherit"
+                      className="size-5 group-hover:size-6 text-inherit group-hover:duration-600"
                     >
                       <path
                         fillRule="evenodd"
@@ -148,7 +126,7 @@ export default function AuthenticatedLayout({ user, header, children }) {
                         clipRule="evenodd"
                       />
                     </svg>
-                    Tasks
+                    <span className="group-hover:duration-600">Tasks</span>
                   </button>
                 </Link>
               </li>
@@ -159,7 +137,7 @@ export default function AuthenticatedLayout({ user, header, children }) {
                   className="block antialiased font-sans  leading-relaxed text-inherit capitalize group"
                 >
                   <button
-                    className="middle none font-sans  center transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-base py-3 rounded-lg text-white group-hover:bg-white/10 active:bg-white/30 w-full flex items-center gap-4 px-4 capitalize"
+                    className="middle none font-sans  center transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-base py-3 rounded-lg text-white group-hover:bg-white/10 group-hover:duration-600 active:bg-white/30 w-full flex items-center gap-4 px-4 capitalize"
                     type="button"
                   >
                     <svg
@@ -167,7 +145,7 @@ export default function AuthenticatedLayout({ user, header, children }) {
                       viewBox="0 0 24 24"
                       fill="currentColor"
                       aria-hidden="true"
-                      className="size-5 group-hover:size-6 text-inherit"
+                      className="size-5 group-hover:size-6 text-inherit group-hover:duration-600"
                     >
                       <path
                         fillRule="evenodd"
@@ -175,7 +153,7 @@ export default function AuthenticatedLayout({ user, header, children }) {
                         clipRule="evenodd"
                       />
                     </svg>
-                    Users
+                    <span className="group-hover:duration-600">Users</span>
                   </button>
                 </Link>
               </li>
@@ -186,7 +164,7 @@ export default function AuthenticatedLayout({ user, header, children }) {
                   className="block antialiased font-sans  leading-relaxed text-inherit capitalize group"
                 >
                   <button
-                    className="middle none font-sans  center transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-base py-3 rounded-lg text-white group-hover:bg-white/10 active:bg-white/30 w-full flex items-center gap-4 px-4 capitalize"
+                    className="middle none font-sans  center transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-base py-3 rounded-lg text-white group-hover:bg-white/10 group-hover:duration-600 active:bg-white/30 w-full flex items-center gap-4 px-4 capitalize"
                     type="button"
                   >
                     <svg
@@ -195,7 +173,7 @@ export default function AuthenticatedLayout({ user, header, children }) {
                       viewBox="0 0 24 24"
                       stroke-width="1.5"
                       stroke="currentColor"
-                      class="size-5 group-hover:size-6 text-inherit"
+                      class="size-5 group-hover:size-6 text-inherit group-hover:duration-600"
                     >
                       <path
                         stroke-linecap="round"
@@ -203,7 +181,7 @@ export default function AuthenticatedLayout({ user, header, children }) {
                         d="M6 6.878V6a2.25 2.25 0 0 1 2.25-2.25h7.5A2.25 2.25 0 0 1 18 6v.878m-12 0c.235-.083.487-.128.75-.128h10.5c.263 0 .515.045.75.128m-12 0A2.25 2.25 0 0 0 4.5 9v.878m13.5-3A2.25 2.25 0 0 1 19.5 9v.878m0 0a2.246 2.246 0 0 0-.75-.128H5.25c-.263 0-.515.045-.75.128m15 0A2.25 2.25 0 0 1 21 12v6a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 18v-6c0-.98.626-1.813 1.5-2.122"
                       />
                     </svg>
-                    My Tasks
+                    <span className="group-hover:duration-600">My Tasks</span>
                   </button>
                 </Link>
               </li>
