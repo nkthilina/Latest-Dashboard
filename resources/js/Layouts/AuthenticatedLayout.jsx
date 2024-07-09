23c35d0b56f386fb4be914b5c5ce000ddd5c1c29
Thilina Madhusanka <120771707+nkthilina@users.noreply.github.com>
2024-07-09 19:59:02 +0530

    106th commit

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -1,9 +1,4 @@
-// import { useState } from "react";
 import React, { useState, useEffect, useRef } from "react";
-// import ApplicationLogo from "@/Components/ApplicationLogo";
-// import Dropdown from "@/Components/Dropdown";
-// import NavLink from "@/Components/NavLink";
-// import ResponsiveNavLink from "@/Components/ResponsiveNavLink";
 import { Link } from "@inertiajs/react";
 
 export default function AuthenticatedLayout({ user, header, children }) {
